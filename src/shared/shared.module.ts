239de446763577be9d43d52bf4d206862653ef56
tranvanhung2003 <tranvanhan29042007@gmail.com>
2025-12-12 11:37:32 +0700

    feat(pipes): register CustomZodValidationPipe as a global pipe and remove unused class-transformer dependency

--- src/shared/shared.module.ts
+++ src/shared/shared.module.ts
@@ -1,45 +1,63 @@
-import {
-  ClassProvider,
-  ClassSerializerInterceptor,
-  Global,
-  Module,
-  Type,
-} from '@nestjs/common';
-import { APP_GUARD, APP_INTERCEPTOR } from '@nestjs/core';
+import { ClassProvider, Global, Module, Type } from '@nestjs/common';
+import { APP_GUARD, APP_INTERCEPTOR, APP_PIPE } from '@nestjs/core';
 import { JwtModule } from '@nestjs/jwt';
+import { ZodSerializerInterceptor } from 'nestjs-zod';
 import { AccessTokenGuard } from './guards/access-token.guard';
 import { ApiKeyGuard } from './guards/api-key.guard';
 import { AuthenticationGuard } from './guards/authentication.guard';
 import { TransformInterceptor } from './interceptors/transform.interceptor';
+import { CustomZodValidationPipe } from './pipes/custom-zod-validation.pipe';
 import { HashingService } from './services/hashing.service';
 import { PrismaService } from './services/prisma.service';
 import { TokenService } from './services/token.service';
 
+// Shared services to be exported
 const sharedServices: Type[] = [PrismaService, HashingService, TokenService];
 
+// Services provided but not exported
 const provideOnlyServices: Type[] = [AccessTokenGuard, ApiKeyGuard];
 
+// Global guards
 const sharedAppGuards: Type[] = [AuthenticationGuard];
 
-// ClassSerializerInterceptor must be last to ensure proper serialization
-// ClassSerializerInterceptor phải nằm ở cuối cùng để đảm bảo việc tuần tự hóa đúng cách
+// Global interceptors
 const sharedAppInterceptors: Type[] = [
   TransformInterceptor,
-  ClassSerializerInterceptor,
+  ZodSerializerInterceptor,
 ];
 
-// Provide guards and interceptors as global providers
+// Global pipes
+const sharedAppPipes: Type[] = [CustomZodValidationPipe];
+
+//
+
+//
+
+//
+
+//
+
+//
+
+// Register global guards
 const appGuards: ClassProvider[] = sharedAppGuards.map((guard) => ({
   provide: APP_GUARD,
   useClass: guard,
 }));
 
+// Register global interceptors
 const appInterceptors: ClassProvider[] = sharedAppInterceptors.map(
   (interceptor) => ({
     provide: APP_INTERCEPTOR,
     useClass: interceptor,
   }),
 );
+
+// Register global pipes
+const appPipes: ClassProvider[] = sharedAppPipes.map((pipe) => ({
+  provide: APP_PIPE,
+  useClass: pipe,
+}));
 
 @Global()
 @Module({
@@ -49,6 +67,7 @@ const appInterceptors: ClassProvider[] = sharedAppInterceptors.map(
     ...provideOnlyServices,
     ...appGuards,
     ...appInterceptors,
+    ...appPipes,
   ],
   exports: [...sharedServices],
 })
