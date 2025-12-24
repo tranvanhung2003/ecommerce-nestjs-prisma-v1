8283623b1e4f4dca4770789ec0c0b2aa7d71badd
tranvanhung2003 <tranvanhan29042007@gmail.com>
2025-12-24 11:27:20 +0700

    feat(auth): add IsPublic decorator to getHello route for enhanced access control

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,11 +1,13 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { IsPublic } from './shared/decorators/auth.decorator';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
+  @IsPublic()
   getHello(): string {
     return this.appService.getHello();
   }
