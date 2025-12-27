5168e939fd0713b0069212ef0937f150b28aa826
tranvanhung2003 <tranvanhan29042007@gmail.com>
2025-12-27 10:56:16 +0700

    fix(module): correct import order and add missing comma in Permission module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,8 +3,8 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './routes/auth/auth.module';
 import { LanguageModule } from './routes/language/language.module';
-import { SharedModule } from './shared/shared.module';
 import { PermissionModule } from './routes/permission/permission.module';
+import { SharedModule } from './shared/shared.module';
 
 @Module({
   imports: [SharedModule, AuthModule, LanguageModule, PermissionModule],
